d4e3c85745b46dd98e6ec9ed0cfa8f8d11712d97
Catherine <bezmozggoff@gmail.com>
2022-10-26 17:01:03 +0700

    uml1

--- src/ru/geekbrains/lessons/ModelStore.java
+++ src/ru/geekbrains/lessons/ModelStore.java
@@ -1,8 +1,21 @@
 package ru.geekbrains.lessons;
 
-public class ModelStore {
+public class ModelStore implements  IModelChanger{
 
     public static void main(String[] args) {
-        
+        private IModelChangeObserver changeObserver;
+        PoligonalModel models;
+        Scene scenes;
+        Flash flashes;
+        Camera cameras;
     }
+
+    public static Scene getScena(int){
+    }
+
+    @Override
+    public void NotifyChange(IModelChanger) {
+    }
+
+
 }
